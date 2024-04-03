3a37eda566d3f6139cf2480138fcbfdc9ec92a34
nvanschaack <nellvanschaack@gmail.com>
2024-04-03 18:45:38 -0500

    polished script.js and added message to randomly selected winner in console

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,18 @@
 // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
 
-// Collect employee data
-//function collectEmployees ()
-const collectEmployees = function () {
-  // This function will allow a user to add multiple employees to display on the page. The user will need to enter the first name, last name, and salary of each employee, then have the option to keep adding employees until they choose to stop. A while loop will be needed here (MDN Web Docs on while loopsLinks to an external site.) The salary will need to be entered as a number, otherwise it should default to $0.
+// TODO: Get user input to create and return an array of employee objects
+// This function will allow a user to add multiple employees to display on the page. The user will need to enter the first name, last name, and salary of each employee, then have the option to keep adding employees until they choose to stop. A while loop will be needed here (MDN Web Docs on while loopsLinks to an external site.) The salary will need to be entered as a number, otherwise it should default to $0.
+// const collectEmployees = function () {
 
-  // start with an empty employee array
-
+function collectEmployees () {
   const allEmployees = []
-
   let dontStop = true
 
   while (dontStop) {
-
     const firstName = prompt(`First Name`);
     const lastName = prompt(`Last Name`);
     const salary = parseInt(prompt(`Salary`));
-
     if (isNaN(salary)) {
       salary = 0
     }
@@ -30,42 +25,39 @@ const collectEmployees = function () {
 
     allEmployees.push(newEmployee)
 
-    dontStop = confirm('do you want to stop')
+    dontStop = confirm('Would you like to add another employee?')
 
   }
 
   return allEmployees
 
-  // TODO: Get user input to create and return an array of employee objects
-
 }
 
-// Display the average salary
-const displayAverageSalary = function (employeesArray) {
-  // This function will take in the generated array of employees and log the average salary and number of employees to the console. You should use a template literal string for this task.
   // TODO: Calculate and display the average salary
+  // This function will take in the generated array of employees and log the average salary and number of employees to the console. You should use a template literal string for this task.
+
+const displayAverageSalary = function (employeesArray) {
+
   let someSum = 0
 
   for (let i = 0; i < employeesArray.length; i++) {
     someSum = someSum + employeesArray[i].salary
- 
   }
 
   const avr = someSum / employeesArray.length
 
   console.log(employeesArray.length);
   console.log(avr);
-
 }
 
-// Select a random employee
-const getRandomEmployee = function (employeesArray) {
-  // This function will take in the generated array of employees, randomly select one employee, and use a template literal to log their full name to the console. The built in Math object can help with random number generation: (MDN Web Docs on Math.randomLinks to an external site.)
   // TODO: Select and display a random employee
+  // This function will take in the generated array of employees, randomly select one employee, and use a template literal to log their full name to the console. The built in Math object can help with random number generation: (MDN Web Docs on Math.randomLinks to an external site.
+
+const getRandomEmployee = function (employeesArray) {
 
   const randomIndex = Math.floor(Math.random() * employeesArray.length)
   console.log(employeesArray[randomIndex]);
-  console.log(`${employeesArray[randomIndex].firstName} ${employeesArray[randomIndex].lastName}`);
+  console.log(`Congradulations to ${employeesArray[randomIndex].firstName} ${employeesArray[randomIndex].lastName}, our random drawing winner!`);
 }
 
 /*
